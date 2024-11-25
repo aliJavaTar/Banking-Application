4b1a2c9082c6a30e28bcbe40d542f7bdf4408975
ali <ali77erfagh99@gmail.com>
2024-11-25 09:17:05 +0330

    add class transfer money and TDD

---
+++ src/main/java/org/bro/banking/domin/usecase/TransferMoney.java
@@ -0,0 +1,4 @@
+package org.bro.banking.domin.usecase;
+
+public class TransferMoney {
+}
---
+++ src/test/java/org/bro/banking/domin/usecase/TransferMoneyShould.java
@@ -0,0 +1,49 @@
+package org.bro.banking.domin.usecase;
+
+import org.junit.jupiter.api.Test;
+
+class TransferMoneyShould {
+
+    @Test
+    void not_transfer_money_if_amount_is_zero() {
+
+    }
+
+    @Test
+    void not_transfer_money_if_source_and_destination_accounts_are_the_same() {
+
+    }
+
+    @Test
+    void not_transfer_money_if_source_account_does_not_exist() {
+
+    }
+
+    @Test
+    void not_transfer_money_if_destination_account_does_not_exist() {
+
+    }
+    @Test
+    void not_transfer_money_if_source_account_has_insufficient_balance() {
+
+    }
+
+    @Test
+    void not_transfer_money_if_source_account_is_not_active() {
+
+    }
+
+    @Test
+    void not_transfer_money_if_destination_account_is_not_active() {
+
+    }
+
+
+
+    @Test
+    void update_source_account_balance_and_update_destination_account_balance_update_transfer_history() {
+        // Given
+        // When
+        // Then
+    }
+}
